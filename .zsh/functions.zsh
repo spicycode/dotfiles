302c94a7de8bf88607cb6ef297b39e10bc0860ef
Chad Humphries <chad.humphries@gmail.com>
2017-07-06 10:20:13 -0400

    Minor fix

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -104,7 +104,7 @@ function homebrew() {
   echo "==> Running 'brew update'" && brew update &&
   echo "==> Running 'brew outdated'" && brew outdated &&
   echo "==> Running 'brew upgrade'" && brew upgrade &&
-  echo "==> Running 'brew cleanup'" && brew cleanup &&
+  echo "==> Running 'brew cleanup'" && brew cleanup
 }
 
 function 2016mbp() {

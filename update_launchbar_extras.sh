3a9dc2d24c76558f46c741adc48d536bd451b9bb
Chad Humphries <chad.humphries@gmail.com>
2017-11-30 10:33:49 -0500

    Updating launchbar plugin script

---
+++ update_launchbar_extras.sh
@@ -0,0 +1,3 @@
+echo "Updating LaunchBar GitHub plugin from https://github.com/bswinnerton/launchbar-github"
+cd ~/Library/Application\ Support/LaunchBar/Actions/github.lbaction/
+git pull origin master

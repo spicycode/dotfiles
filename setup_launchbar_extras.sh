0a3caed6c6b8ba5d3a4ffe41d8d12b716dd1faf8
Chad Humphries <chad.humphries@gmail.com>
2017-11-30 10:28:49 -0500

    Add launchbar extras setup script.sh

---
+++ setup_launchbar_extras.sh
@@ -0,0 +1,3 @@
+echo "Setting up LaunchBar GitHub plugin from https://github.com/bswinnerton/launchbar-github"
+mkdir -p ~/Library/Application\ Support/LaunchBar/Actions/
+git clone https://github.com/bswinnerton/launchbar-github ~/Library/Application\ Support/LaunchBar/Actions/github.lbaction

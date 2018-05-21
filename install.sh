ae5f9893e277aa4989b9dade8bd93da6d5e84943
Chad Humphries <chad@humphries.sh>
2018-05-21 10:37:53 -0400

    Add spaceship prompt to setup

--- install.sh
+++ install.sh
@@ -4,8 +4,9 @@ if [ ! -f "`which brew`" ]; then
   echo "homebrew not installed, run strap first."
 else
   brew bundle
-  echo "Installing pure-prompt for ZSH"
+  echo "Installing prompts for ZSH"
   npm install --global pure-prompt
+  npm install --global spaceship-prompt
 fi
 
 source ./setup_launchbar_extras.sh

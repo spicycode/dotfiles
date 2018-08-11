06ef17afad5c507128a10f6cb79d518eed03669d
Chad Humphries <chad.humphries@gmail.com>
2018-08-11 10:54:26 -0400

    Add more launchbar actions

--- setup_launchbar_extras.sh
+++ setup_launchbar_extras.sh
@@ -1,3 +1,18 @@
+echo "Install http://prenagha.github.io/launchbar/updates.html first"
+echo "Install https://s3.amazonaws.com/renaghan/lbdist/Things.lbaction second"
+echo "Install https://prenagha.github.io/launchbar/dismiss.html third"
+
 echo "Setting up LaunchBar GitHub plugin from https://github.com/bswinnerton/launchbar-github"
+
 mkdir -p ~/Library/Application\ Support/LaunchBar/Actions/
 git clone https://github.com/bswinnerton/launchbar-github ~/Library/Application\ Support/LaunchBar/Actions/github.lbaction
+
+git clone https://github.com/hlissner/lb6-actions ~/Library/Application\ Support/LaunchBar/Actions/
+
+echo "Enabling VPN action"
+ln -s ~/Library/Application\ Support/LaunchBar/Actions/lb6-actions/VPN.lbaction ~/Library/Application\ Support/LaunchBar/Actions/
+
+echo "Enabling Chrome Tabs action"
+ln -s ~/Library/Application\ Support/LaunchBar/Actions/lb6-actions/Chrome\ Tabs.lbaction ~/Library/Application\ Support/LaunchBar/Actions/
+
+

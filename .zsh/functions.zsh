e915252179966f4ffddb36d0db4db173fcad5b21
Chad Humphries <chad@humphries.sh>
2021-10-12 10:04:49 -0400

    Removing unused functions

--- .zsh/functions.zsh
+++ .zsh/functions.zsh
@@ -74,28 +74,6 @@ function trash () {
 }
 
 
-function strip_diff_leading_symbols {
-  local color_code_regex="(\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K])"
-
-  # simplify the unified patch diff header
-  sed -r "s/^($color_code_regex)diff --git .*$//g" | \
-         sed -r "s/^($color_code_regex)index .*$/\n\1$(rule)/g" | \
-         sed -r "s/^($color_code_regex)\+\+\+(.*)$/\1+++\5\n\1$(rule)\x1B\[m/g" |\
- 
-  # actually strips the leading symbols
-  sed -r "s/^($color_code_regex)[\+\-]/\1 /g"
-}
-
-## Print a horizontal rule
-rule () {
-  printf "%$(tput cols)s\n"|tr " " "─"}}
-
-
-function 5k() {
-  SwitchAudioSource -t output -s "Audioengine D1   "
-  SwitchAudioSource -t input -s "LG UltraFine Display Audio"
-}
-
 function add_keys() {
   ssh-add -k 
 }

1b49cdc4f401a2283102d1796a27e2369c51cd30
Chad Humphries <chad@humphries.sh>
2021-11-08 11:28:19 -0500

    format with stylua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,113 +2,105 @@
 local g = vim.g
 local cmd = vim.cmd
 local o, wo, bo = vim.o, vim.wo, vim.bo
-local utils = require('config.utils')
+local utils = require("config.utils")
 local opt = utils.opt
 local autocmd = utils.autocmd
 local map, nmap = utils.map, utils.nmap
-require('plugins') -- Install/Update plugins
+require("plugins") -- Install/Update plugins
 -- Leader/local leader
 g.mapleader = [[,]] -- g.maplocalleader = [[ ]]
 
-
-local telescope = require('telescope')
-telescope.setup {
-  defaults = {
-    layout_strategy = 'flex', 
-    scroll_strategy = 'cycle'
-  },
-  pickers = {
-    find_files = {
-      disable_devicons = true,
-      previewer = false,
-    },
-    file_browser = {
-      disable_devicons = true,
-      previewer = false,
-    }
-  }
-}
-
+local telescope = require("telescope")
+telescope.setup({
+	defaults = {
+		layout_strategy = "flex",
+		scroll_strategy = "cycle",
+	},
+	pickers = {
+		find_files = {
+			disable_devicons = true,
+			previewer = false,
+		},
+		file_browser = {
+			disable_devicons = true,
+			previewer = false,
+		},
+	},
+})
 
 -- Settings
-local buffer = {o, bo}
-local window = {o, wo}
+local buffer = { o, bo }
+local window = { o, wo }
 
+opt("wildmode", "longest,full")
 
-opt('wildmode', 'longest,full')
+opt("inccommand", "nosplit")
 
-opt('inccommand', 'nosplit')
-
-opt('tabstop', 2, buffer)
-opt('softtabstop', 2, buffer)
-opt('expandtab', true, buffer)
-opt('shiftwidth', 2, buffer)
+opt("tabstop", 2, buffer)
+opt("softtabstop", 2, buffer)
+opt("expandtab", true, buffer)
+opt("shiftwidth", 2, buffer)
 
 -- Colorscheme
-opt('termguicolors', true)
-opt('background', 'dark')
-cmd [[colorscheme ayu]]
+opt("termguicolors", true)
+opt("background", "dark")
+cmd([[colorscheme ayu]])
 
 -- Numbers
-opt('number', true)
-opt('numberwidth', 3)
-
+opt("number", true)
+opt("numberwidth", 3)
 
 -- Window Management
 
 -- Open new horizontal split windows below current
-opt('splitbelow', true)
+opt("splitbelow", true)
 
 -- Open new vertical split windows to the right
-opt('splitright', true)
-
+opt("splitright", true)
 
 -- Buffers
 
 -- Buffers become hidden when unloaded
-opt('hidden', true)
-
+opt("hidden", true)
 
 -- Always show status bar
-opt('laststatus', 2)
+opt("laststatus", 2)
 
 -- Enable transparency on floating windows
-opt('winblend', 10)
+opt("winblend", 10)
 
 -- Search:
 -- show the `best match so far' as search strings are typed:
-opt('incsearch', true)
+opt("incsearch", true)
 
 -- Don't highlight search result.
-opt('hlsearch', false)
+opt("hlsearch", false)
 --set nohlsearch
 
 -- Searches wrap around the end of the file
-opt('wrapscan', true)
-
+opt("wrapscan", true)
 
 -- Ignore case on insert completion
-opt('infercase', true)
+opt("infercase", true)
 
 -- Stylin:
 -- Enable transparency on floating windows
-opt('winblend', 10)
+opt("winblend", 10)
 
 -- If lightline/airline is enabled, don't show mode under it
-opt('showmode', false)
+opt("showmode", false)
 
 -- Commands
-cmd [[command! PackerInstall packadd packer.nvim | lua require('plugins').install()]]
-cmd [[command! PackerUpdate packadd packer.nvim | lua require('plugins').update()]]
-cmd [[command! PackerSync packadd packer.nvim | lua require('plugins').sync()]]
-cmd [[command! PackerClean packadd packer.nvim | lua require('plugins').clean()]]
-cmd [[command! PackerCompile packadd packer.nvim | lua require('plugins').compile()]]
+cmd([[command! PackerInstall packadd packer.nvim | lua require('plugins').install()]])
+cmd([[command! PackerUpdate packadd packer.nvim | lua require('plugins').update()]])
+cmd([[command! PackerSync packadd packer.nvim | lua require('plugins').sync()]])
+cmd([[command! PackerClean packadd packer.nvim | lua require('plugins').clean()]])
+cmd([[command! PackerCompile packadd packer.nvim | lua require('plugins').compile()]])
 
 local lspconfig = require("lspconfig")
 
-
 -- Pretty QuickFix
-require('pqf').setup()
+require("pqf").setup()
 
 -- Neovim doesn't support snippets out of the box, so we need to mutate the
 -- capabilities we send to the language server to let them know we want snippets.
@@ -116,42 +108,42 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 -- Compe config
-require('compe').setup {
-  enabled = true,
-  autocomplete = true,
-  debug = false, 
-  min_length = 1,
-  preselect = 'always',
-  documentation = true,
-  source = {
-    calc = false,
-    path = true, 
-    buffer = true, 
-    nvim_lsp = true, 
-    vsnip = false,
-    nvim_lua = true,
-    spell = false,
-    tags = true,
-    treesitter = true
-  }
-}
+require("compe").setup({
+	enabled = true,
+	autocomplete = true,
+	debug = false,
+	min_length = 1,
+	preselect = "always",
+	documentation = true,
+	source = {
+		calc = false,
+		path = true,
+		buffer = true,
+		nvim_lsp = true,
+		vsnip = false,
+		nvim_lua = true,
+		spell = false,
+		tags = true,
+		treesitter = true,
+	},
+})
 
-local compeOpts = {noremap = true, silent = true, expr = true}
-map('i', '<c-c>', [[compe#complete()]], compeOpts)
-map('i', '<cr>', [[compe#confirm('<cr>')]], compeOpts)
-map('i', '<c-e>', [[compe#close('<c-e>')]], compeOpts)
+local compeOpts = { noremap = true, silent = true, expr = true }
+map("i", "<c-c>", [[compe#complete()]], compeOpts)
+map("i", "<cr>", [[compe#confirm('<cr>')]], compeOpts)
+map("i", "<c-e>", [[compe#close('<c-e>')]], compeOpts)
 
 -- Telescope
-nmap('<C-p>', ':Telescope find_files<CR>', {silent = true})
-nmap('<C-]>', ':Telescope tags<CR>', {silent = true})
-nmap('<leader>lb', ':Telescope buffers<CR>', {silent = true})
-nmap('<leader>lt', ':Telescope help_tags<CR>', {silent = true})
-nmap('<leader>f', '<cmd>Telescope live_grep<cr>', {silent = true})
+nmap("<C-p>", ":Telescope find_files<CR>", { silent = true })
+nmap("<C-]>", ":Telescope tags<CR>", { silent = true })
+nmap("<leader>lb", ":Telescope buffers<CR>", { silent = true })
+nmap("<leader>lt", ":Telescope help_tags<CR>", { silent = true })
+nmap("<leader>f", "<cmd>Telescope live_grep<cr>", { silent = true })
 
 -- TABS: Navigation
-nmap('<leader>tp', ':tabprevious<CR>', {silent = true})
-nmap('<leader>tn', ':tabnext<CR>', {silent = true})
-nmap('<leader>te', ':tabedit<space>')
+nmap("<leader>tp", ":tabprevious<CR>", { silent = true })
+nmap("<leader>tn", ":tabnext<CR>", { silent = true })
+nmap("<leader>te", ":tabedit<space>")
 
 -- Close buffers
-nmap('<leader>b', ':bd<CR>')
+nmap("<leader>b", ":bd<CR>")
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,102 +1,104 @@
 local execute = vim.api.nvim_command
 local fn = vim.fn
 
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 
 if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
-  execute 'packadd packer.nvim' end
+	fn.system({ "git", "clone", "https://github.com/wbthomason/packer.nvim", install_path })
+	execute("packadd packer.nvim")
+end
 
-  return require('packer').startup(function()
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
-    use 'christoomey/vim-tmux-navigator'
-    use 'tmux-plugins/vim-tmux-focus-events'
+return require("packer").startup(function()
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
+	use("christoomey/vim-tmux-navigator")
+	use("tmux-plugins/vim-tmux-focus-events")
 
-    use 'tpope/vim-vinegar'
-    use 'watsoncj/vim-ripgrep' 
+	use("tpope/vim-vinegar")
+	use("watsoncj/vim-ripgrep")
 
-    -- Async building & commands
-    use 'tpope/vim-dispatch'
+	-- Async building & commands
+	use("tpope/vim-dispatch")
 
-    use 'tpope/vim-fugitive'
-    use 'tpope/vim-rsi'
-    use 'tpope/vim-git'
-    use 'tpope/vim-sensible'
-    use 'tpope/vim-commentary'
+	use("tpope/vim-fugitive")
+	use("tpope/vim-rsi")
+	use("tpope/vim-git")
+	use("tpope/vim-sensible")
+	use("tpope/vim-commentary")
 
-    -- LSP
-    use 'neovim/nvim-lspconfig'
-    use 'nvim-lua/completion-nvim'
-    use 'nvim-lua/lsp_extensions.nvim'
-    use 'onsails/lspkind-nvim'
-    use 'glepnir/lspsaga.nvim'
-    use 'folke/trouble.nvim'
+	-- LSP
+	use("neovim/nvim-lspconfig")
+	use("nvim-lua/completion-nvim")
+	use("nvim-lua/lsp_extensions.nvim")
+	use("onsails/lspkind-nvim")
+	use("glepnir/lspsaga.nvim")
+	use("folke/trouble.nvim")
 
-    -- Color and Themes
-    -- Highlight CSS Colors
-    use 'norcalli/nvim-colorizer.lua'
-    use 'ayu-theme/ayu-vim'
+	-- Color and Themes
+	-- Highlight CSS Colors
+	use("norcalli/nvim-colorizer.lua")
+	use("ayu-theme/ayu-vim")
 
-    -- Make Quickfix nice
-    use 'https://gitlab.com/yorickpeterse/nvim-pqf.git'
+	-- Make Quickfix nice
+	use("https://gitlab.com/yorickpeterse/nvim-pqf.git")
 
-    -- Language Syntax/etc support
-    use 'euclidianAce/BetterLua.vim'
-    use 'vim-ruby/vim-ruby'
-    use 'tpope/vim-rails'
+	-- Language Syntax/etc support
+	use("euclidianAce/BetterLua.vim")
+	use("vim-ruby/vim-ruby")
+	use("tpope/vim-rails")
 
-    -- elixir
-    use 'vim-erlang/vim-erlang-runtime'
-    use 'vim-erlang/vim-erlang-compiler'
-    use 'vim-erlang/vim-erlang-omnicomplete'
-    use 'elixir-lang/vim-elixir'
-    use 'slashmili/alchemist.vim'
-    use 'avdgaag/vim-phoenix'
+	-- elixir
+	use("vim-erlang/vim-erlang-runtime")
+	use("vim-erlang/vim-erlang-compiler")
+	use("vim-erlang/vim-erlang-omnicomplete")
+	use("elixir-lang/vim-elixir")
+	use("slashmili/alchemist.vim")
+	use("avdgaag/vim-phoenix")
 
-    -- rust
-    use 'rust-lang/rust.vim'
+	-- rust
+	use("rust-lang/rust.vim")
 
-    -- Registers
-    use 'junegunn/vim-peekaboo'
+	-- Registers
+	use("junegunn/vim-peekaboo")
 
-    -- Quickfix
-    -- use {'Olical/vim-enmasse', cmd = 'EnMasse'}
-    use 'kevinhwang91/nvim-bqf'
+	-- Quickfix
+	-- use {'Olical/vim-enmasse', cmd = 'EnMasse'}
+	use("kevinhwang91/nvim-bqf")
 
-    -- Search
-    use {
-      'nvim-telescope/telescope.nvim',
-      requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
-    }
+	-- Search
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = { { "nvim-lua/popup.nvim" }, { "nvim-lua/plenary.nvim" } },
+	})
 
-    -- Terminal
-    use 'voldikss/vim-floaterm'
+	-- Terminal
+	use("voldikss/vim-floaterm")
 
-    -- Highlights
-    use {
-      'nvim-treesitter/nvim-treesitter',
-      requires = {
-        'nvim-treesitter/nvim-treesitter-refactor', 'nvim-treesitter/nvim-treesitter-textobjects'
-      },
-      run = ':TSUpdate'
-    }
+	-- Highlights
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		requires = {
+			"nvim-treesitter/nvim-treesitter-refactor",
+			"nvim-treesitter/nvim-treesitter-textobjects",
+		},
+		run = ":TSUpdate",
+	})
 
-    -- Just for tracking progess until this is ready for use
-    -- use 'mfussenegger/nvim-lint'
+	-- Just for tracking progess until this is ready for use
+	-- use 'mfussenegger/nvim-lint'
 
-    use "hrsh7th/nvim-compe"
-    use "hrsh7th/vim-vsnip"
-    use "hrsh7th/vim-vsnip-integ"
+	use("hrsh7th/nvim-compe")
+	use("hrsh7th/vim-vsnip")
+	use("hrsh7th/vim-vsnip-integ")
 
-    -- Debugger
-    use {'mfussenegger/nvim-dap', opt = true}
+	-- Debugger
+	use({ "mfussenegger/nvim-dap", opt = true })
 
-    use {
-      'puremourning/vimspector',
-      setup = [[vim.g.vimspector_enable_mappings = 'HUMAN']],
-      disable = true
-    }
+	use({
+		"puremourning/vimspector",
+		setup = [[vim.g.vimspector_enable_mappings = 'HUMAN']],
+		disable = true,
+	})
 
-    use "jparise/vim-graphql"
-  end)
+	use("jparise/vim-graphql")
+end)

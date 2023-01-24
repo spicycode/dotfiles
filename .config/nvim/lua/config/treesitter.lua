9727900926a50903afacb71f459f32f855febca9
Chad Humphries <chad@humphries.sh>
2023-01-24 15:29:45 -0500

    add dockerfile and ruby

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -3,7 +3,7 @@ require 'nvim-treesitter.configs'.setup {
   ensure_installed = {
     "c", "lua", "vim", "help", "graphql", "markdown",
     "python", "sql", "json", "typescript", "bash",
-    "tsx"
+    "tsx", "dockerfile", "ruby"
   },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)

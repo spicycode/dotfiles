051f781f0e7549142b7eb595dadec93acc63473f
Chad Humphries <chad@humphries.sh>
2023-01-24 11:57:31 -0500

    simplify and update for 0.8 support)

--- .config/nvim/lua/config/lsp.lua
+++ .config/nvim/lua/config/lsp.lua
@@ -46,7 +46,6 @@ lsp.init = function()
     }
   end
 
-  -- Prerequisite: https://github.com/sumneko/lua-language-server/wiki/Build-and-Run
   require 'lspconfig'.sumneko_lua.setup {
     capabilities = capabilities,
     cmd = cmd,
@@ -119,42 +118,7 @@ lsp.init = function()
     capabilities = capabilities,
     on_attach = on_attach
   }
-
 end
-
-local function goto_definition(split_cmd)
-  local util = vim.lsp.util
-  local log = require("vim.lsp.log")
-  local api = vim.api
-
-  -- note, this handler style is for neovim 0.5.1/0.6, if on 0.5, call with function(_, method, result)
-  local handler = function(_, result, ctx)
-    if result == nil or vim.tbl_isempty(result) then
-      local _ = log.info() and log.info(ctx.method, "No location found")
-      return nil
-    end
-
-    if split_cmd then
-      vim.cmd(split_cmd)
-    end
-
-    if vim.tbl_islist(result) then
-      util.jump_to_location(result[1])
-
-      if #result > 1 then
-        util.set_qflist(util.locations_to_items(result))
-        api.nvim_command("copen")
-        api.nvim_command("wincmd p")
-      end
-    else
-      util.jump_to_location(result)
-    end
-  end
-
-  return handler
-end
-
-vim.lsp.handlers["textDocument/definition"] = goto_definition('split')
 
 require "fidget".setup {
   window = {

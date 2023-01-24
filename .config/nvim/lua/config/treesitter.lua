2d579311bf4b4891dd1dfb3c5ffbafa25a118147
Chad Humphries <chad@humphries.sh>
2023-01-24 14:22:37 -0500

    final tweak

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -11,9 +11,6 @@ require'nvim-treesitter.configs'.setup {
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
 
-  -- List of parsers to ignore installing (for "all")
-  ignore_install = { "javascript" },
-
   highlight = {
     -- `false` will disable the whole extension
     enable = true,

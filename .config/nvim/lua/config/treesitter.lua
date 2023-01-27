93ac105896709009f39b4e8e057383578256edf9
Chad Humphries <chad@humphries.sh>
2023-01-27 14:35:12 -0500

    Javascript treesitter

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -3,7 +3,7 @@ require 'nvim-treesitter.configs'.setup {
   ensure_installed = {
     "c", "lua", "vim", "help", "graphql", "markdown",
     "python", "sql", "json", "typescript", "bash",
-    "tsx", "dockerfile", "ruby", "gitcommit"
+    "tsx", "dockerfile", "ruby", "gitcommit", "javascript"
   },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)

702b54bb83b71d76a6b33cb275e4ff94276ee992
Chad Humphries <chad@humphries.sh>
2023-01-27 14:25:14 -0500

    Migrate to theming file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,35 +2,7 @@
 vim.g.mapleader = ',' -- make sure to set `mapleader` before lazy so your mappings are correct
 
 require("config.plugins")
-
-require("catppuccin").setup({
-  cmp = true,
-  fidget = true,
-  markdown = true,
-  mason = true,
-  native_lsp = {
-    enabled = true,
-    virtual_text = {
-      errors = { "italic" },
-      hints = { "italic" },
-      warnings = { "italic" },
-      information = { "italic" },
-    },
-    underlines = {
-      errors = { "underline" },
-      hints = { "underline" },
-      warnings = { "underline" },
-      information = { "underline" },
-    },
-  },
-  lsp_saga = true,
-  lsp_trouble = true,
-  treesitter = true
-})
-
-vim.cmd.colorscheme "catppuccin-mocha"
-vim.cmd.syntax = "on"
-
+require("config.theming")
 require("config.options")
 require("config.plugins")
 require("config.telescope")

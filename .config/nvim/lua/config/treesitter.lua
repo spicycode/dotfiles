bb525ac8e2f537f3590029eeb23adbf6dfb84159
Chad Humphries <chad@humphries.sh>
2023-02-21 08:55:43 -0500

    Try to fix treesitter reinstall

--- .config/nvim/lua/config/treesitter.lua
+++ .config/nvim/lua/config/treesitter.lua
@@ -14,6 +14,9 @@ require 'nvim-treesitter.configs'.setup {
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = false,
 
+  -- Attempting to fix  constant reinstalls https://www.reddit.com/r/neovim/comments/110x08m/treesitter_keeps_recompiling_parsers/
+  version = nil,
+
   highlight = {
     -- `false` will disable the whole extension
     enable = true,

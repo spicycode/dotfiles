bc94aab17f4f19e1d28cc84b587a27fe300ef9ac
Chad Humphries <chad@humphries.sh>
2023-03-22 21:01:46 -0400

    zsh aliases for exa

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,8 +1,10 @@
+
 # Colorize output, add file type indicator, and put sizes in human readable format
-alias ls='ls -GFh --color'
+# alias ls='ls -GFh --color'
+alias ls='exa  --color-scale --icons'
 
 # Same as above, but in long listing format
-alias ll='ls -GFhl --color'
+alias ll='exa --color-scale --icons -l'
 
 alias git='hub'
 alias vim='nvim'

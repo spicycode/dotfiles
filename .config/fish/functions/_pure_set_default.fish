97dd7bbb05700646e759104841473925e96e1e7c
Chad Humphries <chad@humphries.sh>
2023-03-30 16:23:53 -0400

    remove fish config

--- .config/fish/functions/_pure_set_default.fish
+++
@@ -1,8 +0,0 @@
-function _pure_set_default \
-    --description 'Set default value for configuration variable' \
-    --argument-names var default
-
-    if not set -q --universal $var
-        set --universal $var $default
-    end
-end
--- .config/fish/functions/homebrew.fish
+++
@@ -1,6 +0,0 @@
-function homebrew
-  echo "==> Running 'brew update'" && brew update &&
-  echo "==> Running 'brew outdated'" && brew outdated &&
-  echo "==> Running 'brew upgrade'" && brew upgrade &&
-  echo "==> Running 'brew cleanup'" && brew cleanup
-end

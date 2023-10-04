1356a8f0dde7486c99f025af72219a6c134d7a87
Chad Humphries <chad@humphries.sh>
2023-10-04 18:51:04 -0400

    Run go fmt + go import

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -2,6 +2,7 @@ local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
   pattern = "*.go",
   callback = function()
+  -- runs go fmt and go import
    require('go.format').goimport()
   end,
   group = format_sync_grp,

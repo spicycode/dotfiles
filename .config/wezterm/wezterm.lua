26beb55316489d9756e4d9e6bae68956b7d77c3a
Chad Humphries <chad@humphries.sh>
2023-12-05 14:02:24 -0500

    fix color scheme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,159 @@
-local wezterm = require("wezterm")
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
 -- local is_dark = wezterm.gui.get_appearance():find("Dark")
 local is_dark = true
+
+local colors = {
+  peach = "#FFDAB9",
+  green = "#66b032"
+}
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.font = wezterm.font_with_fallback({
+  "Liga SFMono Nerd Font",
+  "Apple Color Emoji",
+})
+
+config.font_size = 16
+config.max_fps = 120
+config.enable_wayland = false
+config.pane_focus_follows_mouse = false
+config.warn_about_missing_glyphs = false
+config.show_update_window = true
+config.check_for_updates = true
+config.line_height = 1.5
+config.window_decorations = "RESIZE"
+config.window_close_confirmation = "NeverPrompt"
+config.audible_bell = "Disabled"
+config.window_padding = {
+  left = 5,
+  right = 5,
+  top = 5,
+  bottom = 0,
+}
+config.initial_cols = 170
+config.initial_rows = 30
+
+config.inactive_pane_hsb = {
+  saturation = 1.0,
+  brightness = is_dark and 0.85 or 0.95,
+}
+
+config.enable_scroll_bar = false
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.show_new_tab_button_in_tab_bar = false
+config.window_background_opacity = 1.0
+config.tab_max_width = 50
+config.hide_tab_bar_if_only_one_tab = false
+config.disable_default_key_bindings = false
+config.front_end = "WebGpu"
+config.color_scheme = 'Catppuccin Mocha'
+
+config.keys = {
+  {
+    mods = "ALT",
+    key = [[\]],
+    action = wezterm.action({
+      SplitHorizontal = { domain = "CurrentPaneDomain" },
+    }),
+  },
+  {
+    mods = "ALT|SHIFT",
+    key = [[|]],
+    action = wezterm.action.SplitPane({
+      top_level = true,
+      direction = "Right",
+      size = { Percent = 50 },
+    }),
+  },
+  {
+    mods = "ALT",
+    key = [[-]],
+    action = wezterm.action({
+      SplitVertical = { domain = "CurrentPaneDomain" },
+    }),
+  },
+  {
+    mods = "ALT|SHIFT",
+    key = [[_]],
+    action = wezterm.action.SplitPane({
+      top_level = true,
+      direction = "Down",
+      size = { Percent = 50 },
+    }),
+  },
+  {
+    key = "n",
+    mods = "ALT",
+    action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }),
+  },
+  {
+    key = "Q",
+    mods = "ALT",
+    action = wezterm.action({ CloseCurrentTab = { confirm = false } }),
+  },
+  { key = "q", mods = "ALT",        action = wezterm.action.CloseCurrentPane({ confirm = false }) },
+  { key = "z", mods = "ALT",        action = wezterm.action.TogglePaneZoomState },
+  { key = "h", mods = "ALT|SHIFT",  action = wezterm.action.AdjustPaneSize({ "Left", 1 }) },
+  { key = "j", mods = "ALT|SHIFT",  action = wezterm.action.AdjustPaneSize({ "Down", 1 }) },
+  { key = "k", mods = "ALT|SHIFT",  action = wezterm.action.AdjustPaneSize({ "Up", 1 }) },
+  { key = "l", mods = "ALT|SHIFT",  action = wezterm.action.AdjustPaneSize({ "Right", 1 }) },
+  { key = "h", mods = "ALT",        action = wezterm.action.EmitEvent("ActivatePaneDirection-left") },
+  { key = "j", mods = "ALT",        action = wezterm.action.EmitEvent("ActivatePaneDirection-down") },
+  { key = "k", mods = "ALT",        action = wezterm.action.EmitEvent("ActivatePaneDirection-up") },
+  { key = "l", mods = "ALT",        action = wezterm.action.EmitEvent("ActivatePaneDirection-right") },
+  { key = "[", mods = "ALT",        action = wezterm.action({ ActivateTabRelative = -1 }) },
+  { key = "]", mods = "ALT",        action = wezterm.action({ ActivateTabRelative = 1 }) },
+  { key = "{", mods = "SHIFT|ALT",  action = wezterm.action.MoveTabRelative(-1) },
+  { key = "}", mods = "SHIFT|ALT",  action = wezterm.action.MoveTabRelative(1) },
+  { key = "v", mods = "ALT",        action = wezterm.action.ActivateCopyMode },
+  { key = "c", mods = "CTRL|SHIFT", action = wezterm.action({ CopyTo = "Clipboard" }) },
+  { key = "v", mods = "CTRL|SHIFT", action = wezterm.action({ PasteFrom = "Clipboard" }) },
+  { key = "=", mods = "CTRL",       action = wezterm.action.IncreaseFontSize },
+  { key = "-", mods = "CTRL",       action = wezterm.action.DecreaseFontSize },
+  { key = "1", mods = "ALT",        action = wezterm.action({ ActivateTab = 0 }) },
+  { key = "2", mods = "ALT",        action = wezterm.action({ ActivateTab = 1 }) },
+  { key = "3", mods = "ALT",        action = wezterm.action({ ActivateTab = 2 }) },
+  { key = "4", mods = "ALT",        action = wezterm.action({ ActivateTab = 3 }) },
+  { key = "5", mods = "ALT",        action = wezterm.action({ ActivateTab = 4 }) },
+  { key = "6", mods = "ALT",        action = wezterm.action({ ActivateTab = 5 }) },
+  { key = "7", mods = "ALT",        action = wezterm.action({ ActivateTab = 6 }) },
+  { key = "8", mods = "ALT",        action = wezterm.action({ ActivateTab = 7 }) },
+  { key = "9", mods = "ALT",        action = wezterm.action({ ActivateTab = 8 }) },
+}
+
+config.hyperlink_rules = {
+  {
+    regex = "\\b\\w+://[\\w.-]+:[0-9]{2,15}\\S*\\b",
+    format = "$0",
+  },
+  {
+    regex = "\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b",
+    format = "$0",
+  },
+  {
+    regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
+    format = "mailto:$0",
+  },
+  {
+    regex = [[\bfile://\S*\b]],
+    format = "$0",
+  },
+  {
+    regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
+    format = "$0",
+  },
+}
 
 local function is_vi_process(pane)
   return pane:get_foreground_process_name():find("n?vim") ~= nil
@@ -30,75 +183,14 @@ wezterm.on("ActivatePaneDirection-down", function(window, pane)
   conditional_activate_pane(window, pane, "Down", "j")
 end)
 
-local catppuccin = {
-  dark = {
-    rosewater = "#F5E0DC",
-    flamingo = "#F2CDCD",
-    pink = "#F5C2E7",
-    mauve = "#CBA6F7",
-    red = "#F38BA8",
-    maroon = "#EBA0AC",
-    peach = "#FAB387",
-    yellow = "#F9E2AF",
-    green = "#A6E3A1",
-    teal = "#94E2D5",
-    sky = "#89DCEB",
-    sapphire = "#74C7EC",
-    blue = "#89B4FA",
-    lavender = "#B4BEFE",
-    text = "#CDD6F4",
-    subtext1 = "#BAC2DE",
-    subtext0 = "#A6ADC8",
-    overlay2 = "#9399B2",
-    overlay1 = "#7F849C",
-    overlay0 = "#6C7086",
-    surface2 = "#585B70",
-    surface1 = "#45475A",
-    surface0 = "#313244",
-    base = "#1E1E2E",
-    mantle = "#181825",
-    crust = "#11111B",
-  },
-  light = {
-    rosewater = "#dc8a78",
-    flamingo = "#DD7878",
-    pink = "#ea76cb",
-    mauve = "#8839EF",
-    red = "#D20F39",
-    maroon = "#E64553",
-    peach = "#FE640B",
-    yellow = "#df8e1d",
-    green = "#40A02B",
-    teal = "#179299",
-    sky = "#04A5E5",
-    sapphire = "#209FB5",
-    blue = "#1e66f5",
-    lavender = "#7287FD",
-    text = "#4C4F69",
-    subtext1 = "#5C5F77",
-    subtext0 = "#6C6F85",
-    overlay2 = "#7C7F93",
-    overlay1 = "#8C8FA1",
-    overlay0 = "#9CA0B0",
-    surface2 = "#ACB0BE",
-    surface1 = "#BCC0CC",
-    surface0 = "#CCD0DA",
-    base = "#FFFFFF",
-    mantle = "#E6E9EF",
-    crust = "#DCE0E8",
-  },
-}
-
-local colors = is_dark and catppuccin.dark or catppuccin.light
-
 local function get_process(tab)
   local process_icons = {
     ["docker"] = {
-      { Foreground = { Color = colors.blue } },
+      { Foreground = { AnsiColor = 'Blue' } },
       { Text = wezterm.nerdfonts.linux_docker },
     },
     ["docker-compose"] = {
-      { Foreground = { Color = colors.blue } },
+      { Foreground = { AnsiColor = 'Blue' } },
       { Text = wezterm.nerdfonts.linux_docker },
     },
     ["nvim"] = {
@@ -118,43 +210,34 @@ local function get_process(tab)
       { Text = wezterm.nerdfonts.dev_terminal },
     },
     ["bash"] = {
-      { Foreground = { Color = colors.subtext0 } },
+      { Foreground = { Color = colors.peach } },
       { Text = wezterm.nerdfonts.cod_terminal_bash },
     },
     ["htop"] = {
-      { Foreground = { Color = colors.yellow } },
       { Text = wezterm.nerdfonts.mdi_chart_donut_variant },
     },
     ["btop"] = {
-      { Foreground = { Color = colors.yellow } },
       { Text = wezterm.nerdfonts.mdi_chart_donut_variant },
     },
     ["cargo"] = {
-      { Foreground = { Color = colors.peach } },
       { Text = wezterm.nerdfonts.dev_rust },
     },
     ["go"] = {
-      { Foreground = { Color = colors.sapphire } },
       { Text = wezterm.nerdfonts.mdi_language_go },
     },
     ["git"] = {
-      { Foreground = { Color = colors.peach } },
       { Text = wezterm.nerdfonts.dev_git },
     },
     ["lua"] = {
-      { Foreground = { Color = colors.blue } },
       { Text = wezterm.nerdfonts.seti_lua },
     },
     ["wget"] = {
-      { Foreground = { Color = colors.yellow } },
       { Text = wezterm.nerdfonts.mdi_arrow_down_box },
     },
     ["curl"] = {
-      { Foreground = { Color = colors.yellow } },
       { Text = wezterm.nerdfonts.mdi_flattr },
     },
     ["gh"] = {
-      { Foreground = { Color = colors.mauve } },
       { Text = wezterm.nerdfonts.dev_github_badge },
     },
   }
@@ -184,7 +267,6 @@ wezterm.on("format-tab-title", function(tab)
     { Text = get_process(tab) },
     { Text = " " },
     { Text = get_current_working_dir(tab) },
-    -- { Foreground = { Color = colors.base } },
     { Text = "  ▕" },
   })
 end)
@@ -196,201 +278,4 @@ wezterm.on("update-right-status", function(window)
   }))
 end)
 
-return {
-  font = wezterm.font_with_fallback({
-    "Liga SFMono Nerd Font",
-    "Apple Color Emoji",
-  }),
-  font_size = 16,
-  max_fps = 120,
-  enable_wayland = false,
-  pane_focus_follows_mouse = false,
-  warn_about_missing_glyphs = false,
-  show_update_window = true,
-  check_for_updates = true,
-  line_height = 1.5,
-  window_decorations = "RESIZE",
-  window_close_confirmation = "NeverPrompt",
-  audible_bell = "Disabled",
-  window_padding = {
-    left = 5,
-    right = 5,
-    top = 5,
-    bottom = 0,
-  },
-  initial_cols = 170,
-  initial_rows = 30,
-  inactive_pane_hsb = {
-    saturation = 1.0,
-    brightness = is_dark and 0.85 or 0.95,
-  },
-  enable_scroll_bar = false,
-  tab_bar_at_bottom = true,
-  use_fancy_tab_bar = false,
-  show_new_tab_button_in_tab_bar = false,
-  window_background_opacity = 1.0,
-  tab_max_width = 50,
-  hide_tab_bar_if_only_one_tab = false,
-  disable_default_key_bindings = false,
-  front_end = "WebGpu",
-  color_scheme = 'Catppuccin Mocha',
-
-  colors = {
-    -- 	split = colors.surface0,
-    -- 	foreground = colors.text,
-    -- 	background = colors.base,
-    -- 	cursor_bg = colors.rosewater,
-    -- 	cursor_border = colors.rosewater,
-    -- 	cursor_fg = is_dark and colors.base or colors.crust,
-    -- 	selection_bg = colors.surface2,
-    -- 	selection_fg = colors.text,
-    -- 	visual_bell = colors.surface0,
-    -- 	indexed = {
-    -- 		[16] = colors.peach,
-    -- 		[17] = colors.rosewater,
-    -- 	},
-    -- 	scrollbar_thumb = colors.surface2,
-    -- 	compose_cursor = colors.flamingo,
-    -- 	ansi = {
-    -- 		is_dark and colors.subtext1 or colors.surface1,
-    -- 		colors.red,
-    -- 		colors.green,
-    -- 		colors.yellow,
-    -- 		colors.blue,
-    -- 		colors.pink,
-    -- 		colors.teal,
-    -- 		is_dark and colors.surface2 or colors.subtext1,
-    -- 	},
-    -- 	brights = {
-    -- 		is_dark and colors.subtext0 or colors.surface2,
-    -- 		colors.red,
-    -- 		colors.green,
-    -- 		colors.yellow,
-    -- 		colors.blue,
-    -- 		colors.pink,
-    -- 		colors.teal,
-    -- 		is_dark and colors.surface1 or colors.subtext0,
-    -- 	},
-    -- 	tab_bar = {
-    -- 		background = colors.crust,
-    -- 		active_tab = {
-    -- 			bg_color = "none",
-    -- 			fg_color = colors.subtext1,
-    -- 			intensity = "Bold",
-    -- 			underline = "None",
-    -- 			italic = false,
-    -- 			strikethrough = false,
-    -- 		},
-    -- 		inactive_tab = {
-    -- 			bg_color = colors.crust,
-    -- 			fg_color = colors.surface2,
-    -- 		},
-    -- 		inactive_tab_hover = {
-    -- 			bg_color = colors.mantle,
-    -- 			fg_color = colors.subtext0,
-    -- 		},
-    -- 		new_tab = {
-    -- 			bg_color = colors.crust,
-    -- 			fg_color = colors.subtext0,
-    -- 		},
-    -- 		new_tab_hover = {
-    -- 			bg_color = colors.crust,
-    -- 			fg_color = colors.subtext0,
-    -- 		},
-    -- 	},
-  },
-  keys = {
-    {
-      mods = "ALT",
-      key = [[\]],
-      action = wezterm.action({
-        SplitHorizontal = { domain = "CurrentPaneDomain" },
-      }),
-    },
-    {
-      mods = "ALT|SHIFT",
-      key = [[|]],
-      action = wezterm.action.SplitPane({
-        top_level = true,
-        direction = "Right",
-        size = { Percent = 50 },
-      }),
-    },
-    {
-      mods = "ALT",
-      key = [[-]],
-      action = wezterm.action({
-        SplitVertical = { domain = "CurrentPaneDomain" },
-      }),
-    },
-    {
-      mods = "ALT|SHIFT",
-      key = [[_]],
-      action = wezterm.action.SplitPane({
-        top_level = true,
-        direction = "Down",
-        size = { Percent = 50 },
-      }),
-    },
-    {
-      key = "n",
-      mods = "ALT",
-      action = wezterm.action({ SpawnTab = "CurrentPaneDomain" }),
-    },
-    {
-      key = "Q",
-      mods = "ALT",
-      action = wezterm.action({ CloseCurrentTab = { confirm = false } }),
-    },
-    { key = "q", mods = "ALT",        action = wezterm.action.CloseCurrentPane({ confirm = false }) },
-    { key = "z", mods = "ALT",        action = wezterm.action.TogglePaneZoomState },
-    { key = "h", mods = "ALT|SHIFT",  action = wezterm.action.AdjustPaneSize({ "Left", 1 }) },
-    { key = "j", mods = "ALT|SHIFT",  action = wezterm.action.AdjustPaneSize({ "Down", 1 }) },
-    { key = "k", mods = "ALT|SHIFT",  action = wezterm.action.AdjustPaneSize({ "Up", 1 }) },
-    { key = "l", mods = "ALT|SHIFT",  action = wezterm.action.AdjustPaneSize({ "Right", 1 }) },
-    { key = "h", mods = "ALT",        action = wezterm.action.EmitEvent("ActivatePaneDirection-left") },
-    { key = "j", mods = "ALT",        action = wezterm.action.EmitEvent("ActivatePaneDirection-down") },
-    { key = "k", mods = "ALT",        action = wezterm.action.EmitEvent("ActivatePaneDirection-up") },
-    { key = "l", mods = "ALT",        action = wezterm.action.EmitEvent("ActivatePaneDirection-right") },
-    { key = "[", mods = "ALT",        action = wezterm.action({ ActivateTabRelative = -1 }) },
-    { key = "]", mods = "ALT",        action = wezterm.action({ ActivateTabRelative = 1 }) },
-    { key = "{", mods = "SHIFT|ALT",  action = wezterm.action.MoveTabRelative(-1) },
-    { key = "}", mods = "SHIFT|ALT",  action = wezterm.action.MoveTabRelative(1) },
-    { key = "v", mods = "ALT",        action = wezterm.action.ActivateCopyMode },
-    { key = "c", mods = "CTRL|SHIFT", action = wezterm.action({ CopyTo = "Clipboard" }) },
-    { key = "v", mods = "CTRL|SHIFT", action = wezterm.action({ PasteFrom = "Clipboard" }) },
-    { key = "=", mods = "CTRL",       action = wezterm.action.IncreaseFontSize },
-    { key = "-", mods = "CTRL",       action = wezterm.action.DecreaseFontSize },
-    { key = "1", mods = "ALT",        action = wezterm.action({ ActivateTab = 0 }) },
-    { key = "2", mods = "ALT",        action = wezterm.action({ ActivateTab = 1 }) },
-    { key = "3", mods = "ALT",        action = wezterm.action({ ActivateTab = 2 }) },
-    { key = "4", mods = "ALT",        action = wezterm.action({ ActivateTab = 3 }) },
-    { key = "5", mods = "ALT",        action = wezterm.action({ ActivateTab = 4 }) },
-    { key = "6", mods = "ALT",        action = wezterm.action({ ActivateTab = 5 }) },
-    { key = "7", mods = "ALT",        action = wezterm.action({ ActivateTab = 6 }) },
-    { key = "8", mods = "ALT",        action = wezterm.action({ ActivateTab = 7 }) },
-    { key = "9", mods = "ALT",        action = wezterm.action({ ActivateTab = 8 }) },
-  },
-  hyperlink_rules = {
-    {
-      regex = "\\b\\w+://[\\w.-]+:[0-9]{2,15}\\S*\\b",
-      format = "$0",
-    },
-    {
-      regex = "\\b\\w+://[\\w.-]+\\.[a-z]{2,15}\\S*\\b",
-      format = "$0",
-    },
-    {
-      regex = [[\b\w+@[\w-]+(\.[\w-]+)+\b]],
-      format = "mailto:$0",
-    },
-    {
-      regex = [[\bfile://\S*\b]],
-      format = "$0",
-    },
-    {
-      regex = [[\b\w+://(?:[\d]{1,3}\.){3}[\d]{1,3}\S*\b]],
-      format = "$0",
-    },
-  },
-}
+return config

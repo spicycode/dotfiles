283d72532c132c79a9ad1fe030c4a758fa889e67
Chad Humphries <chad@humphries.sh>
2024-01-26 14:48:05 -0500

    cleaner ls/ll

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,7 +1,9 @@
 # Colorize output, add file type indicator, and put sizes in human readable format
-alias ls='ls -GFh --color'
+# alias ls='ls -GFh --color'
+alias ls='exa --group-directories-first --icons'
 # Same as above, but in long listing format
-alias ll='ls -GFhl --color'
+# alias ll='ls -GFhl --color'
+alias ll='exa --group-directories-first -l --icons --git'
 
 alias vim='nvim'
 alias code='code-insiders'

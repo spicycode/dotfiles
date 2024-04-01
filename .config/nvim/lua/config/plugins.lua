f913ecc02b8a03c2381656060f896e3978199e16
Chad Humphries <chad@humphries.sh>
2024-04-01 08:52:31 -0400

    Trouble beta

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -121,16 +121,40 @@ return require("lazy").setup({
   { "jparise/vim-graphql" },
   {
     "folke/trouble.nvim",
-    dependencies = {
-      "kyazdani42/nvim-web-devicons"
+    branch = "dev", -- IMPORTANT!
+    keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
     },
-    config = function()
-      require("trouble").setup {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-      }
-    end
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
   },
   { "jremmen/vim-ripgrep" },
   { "kyazdani42/nvim-web-devicons" },

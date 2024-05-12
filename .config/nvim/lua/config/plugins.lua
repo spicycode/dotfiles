494d93c45902259adbaa4bf43d2ab0b2a80adc4a
Chad Humphries <chad@humphries.sh>
2024-05-12 11:30:15 -0400

    Enable gh and gh-user hover

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -61,8 +61,8 @@ return require("lazy").setup({
         init = function()
           -- Require providers
           require("hover.providers.lsp")
-          -- require('hover.providers.gh')
-          -- require('hover.providers.gh_user')
+          require('hover.providers.gh')
+          require('hover.providers.gh_user')
           -- require('hover.providers.jira')
           -- require('hover.providers.dap')
           -- require('hover.providers.man')

aa15cdfe600d345cf98253c7265918684a8eae16
Chad Humphries <chad@humphries.sh>
2024-06-18 08:00:56 -0400

    Default setup for asdf

--- macos-install.sh
+++ macos-install.sh
@@ -4,7 +4,6 @@ if [ ! -f "`which brew`" ]; then
   echo "homebrew not installed, run strap first."
 else
   brew bundle
-  luarocks install --server=https://luarocks.org/dev luaformatter
 fi
 
 source ./perform_default_writes.sh

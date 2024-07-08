1d192b6ce7bed56ea8af77c23da58afb22912c12
Chad Humphries <chad@humphries.sh>
2024-07-08 12:09:11 -0400

    Remove macos-install

--- macos-install.sh
+++
@@ -1,9 +0,0 @@
-echo "Setting up spicycode/dotfiles"
-
-if [ ! -f "`which brew`" ]; then 
-  echo "homebrew not installed, run strap first."
-else
-  brew bundle
-fi
-
-source ./perform_default_writes.sh

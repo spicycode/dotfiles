aa18cd22edfa4cda952e1d91e7c046784439a506
Chad Humphries <chad@humphries.sh>
2024-08-18 16:43:21 -0400

    Trying out avante

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -266,5 +266,35 @@ return require("lazy").setup({
       { "<C-k>", function() require("wezterm-move").move "k" end },
       { "<C-l>", function() require("wezterm-move").move "l" end },
     },
+  },
+  {
+    "yetone/avante.nvim",
+    event = "VeryLazy",
+    build = "make",
+    config = {
+      provider = "openai"
+    },
+    opts = {
+      -- add any opts here
+    },
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      {
+        "grapp-dev/nui-components.nvim",
+        dependencies = {
+          "MunifTanjim/nui.nvim"
+        }
+      },
+      --- The below is optional, make sure to setup it properly if you have lazy=true
+      {
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
   }
 })

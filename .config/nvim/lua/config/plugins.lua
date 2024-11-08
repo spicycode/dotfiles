aa3494e46632251e7c1505ff6e7ac7172358a9f9
Chad Humphries <chad@humphries.sh>
2024-11-08 11:19:27 -0500

    grug far

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -395,5 +395,29 @@ return require("lazy").setup({
 			"nvchad/menu",
 			lazy = true,
 		},
+		{
+			"MagicDuck/grug-far.nvim",
+			opts = { headerMaxWidth = 80 },
+			keys = {
+				{
+					"\\",
+					function()
+						local grug = require("grug-far")
+						local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+						grug.open({
+							transient = true,
+							prefills = {
+								filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+							},
+						})
+					end,
+					mode = { "n", "v" },
+					desc = "Search and Replace",
+				},
+			},
+			config = function()
+				require("grug-far").setup()
+			end,
+		},
 	},
 })

51067aff989986b2527565482b3c47ac0c032f0a
Chad Humphries <chad@humphries.sh>
2024-12-16 19:33:19 -0500

    ecolog

--- .config/nvim/lua/config/plugins.lua
+++ .config/nvim/lua/config/plugins.lua
@@ -175,15 +175,35 @@ return require("lazy").setup({
 			},
 		},
 		{ "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
-		{ -- optional cmp completion source for require statements and module annotations
-			"hrsh7th/nvim-cmp",
-			opts = function(_, opts)
-				opts.sources = opts.sources or {}
-				table.insert(opts.sources, {
-					name = "lazydev",
-					group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-				})
-			end,
+		{
+			"philosofonusus/ecolog.nvim",
+			lazy = false,
+			opts = {
+				integrations = {
+					-- WARNING: for both cmp integrations see readme section below
+					nvim_cmp = false, -- If you dont plan to use nvim_cmp set to false, enabled by default
+					-- If you are planning to use blink cmp uncomment this line
+					blink_cmp = true,
+					lspsaga = true,
+				},
+				-- Enables shelter mode for sensitive values
+				shelter = {
+					configuration = {
+						partial_mode = false, -- false by default, disables partial mode, for more control check out shelter partial mode
+						mask_char = "*", -- Character used for masking
+					},
+					modules = {
+						cmp = true, -- Mask values in completion
+						peek = false, -- Mask values in peek view
+						files = false, -- Mask values in files
+						telescope = false, -- Mask values in telescope
+					},
+				},
+				-- true by default, enables built-in types (database_url, url, etc.)
+				types = true,
+				path = vim.fn.getcwd(), -- Path to search for .env files
+				preferred_environment = "development", -- Optional: prioritize specific env files
+			},
 		},
 		{
 			"saghen/blink.cmp",
@@ -211,6 +231,7 @@ return require("lazy").setup({
 						-- dont show LuaLS require statements when lazydev has items
 						lsp = { fallback_for = { "lazydev" } },
 						lazydev = { name = "LazyDev", module = "lazydev.integrations.blink" },
+						ecolog = { name = "ecolog", module = "ecolog.integrations.cmp.blink_cmp" },
 					},
 				},
 			},
--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,4 +1,5 @@
 local telescope = require("telescope")
+require("telescope").load_extension("ecolog")
 
 telescope.setup({
 	defaults = {
@@ -45,6 +46,23 @@ telescope.setup({
 			require("telescope.themes").get_dropdown({
 				-- even more opts
 			}),
+		},
+		ecolog = {
+			shelter = {
+				-- Whether to show masked values when copying to clipboard
+				mask_on_copy = false,
+			},
+			-- Default keybindings
+			mappings = {
+				-- Key to copy value to clipboard
+				copy_value = "<C-y>",
+				-- Key to copy name to clipboard
+				copy_name = "<C-n>",
+				-- Key to append value to buffer
+				append_value = "<C-a>",
+				-- Key to append name to buffer (defaults to <CR>)
+				append_name = "<CR>",
+			},
 		},
 	},
 })

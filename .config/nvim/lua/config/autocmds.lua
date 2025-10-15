9ccbc3dcc1ab04fc985563adc08ba00afd8cb8db
Chad Humphries <chad@humphries.sh>
2025-10-15 09:34:25 -0400

    Adding autocmd group to nowrap quickfix list

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -1,9 +1,25 @@
 local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
 vim.api.nvim_create_autocmd("BufWritePre", {
 	pattern = "*.go",
+	desc = "Autoformat go files pre save",
 	callback = function()
 		-- runs go fmt and go import
 		require("go.format").goimport()
 	end,
 	group = format_sync_grp,
 })
+
+local quickfix_grp = vim.api.nvim_create_augroup("quickfix", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "qf",
+	desc = "Set nowrap on Quickfix",
+	callback = function()
+		vim.wo.wrap = false
+	end,
+	group = quickfix_grp,
+})
+
+-- augroup quickfix
+--     autocmd!
+--     autocmd FileType qf setlocal wrap
+-- augroup END

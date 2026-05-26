00b7237da9e7f841357f1301d75ad7a0302d867c
Chad Humphries <chad@humphries.sh>
2026-05-25 19:39:55 -0700

    Copying default which-key helix prefix, moving to middle of screen

--- .config/nvim/plugin/whichkey.lua
+++ .config/nvim/plugin/whichkey.lua
@@ -1,35 +1,48 @@
 require("lazyload").on_vim_enter(function()
-  vim.pack.add({
-    { src = "https://github.com/folke/which-key.nvim", version = vim.version.range("*") },
-  })
+	vim.pack.add({
+		{ src = "https://github.com/folke/which-key.nvim", version = vim.version.range("*") },
+	})
 
-  require("which-key").setup({
-    preset = "helix",
-  })
+	require("which-key").setup({
+		-- based on preset = "helix",
+		win = {
+			width = { min = 30, max = 60 },
+			height = { min = 4, max = 0.75 },
+			padding = { 0, 1 },
+			col = -1,
+			row = 11,
+			border = "rounded",
+			title = true,
+			title_pos = "left",
+		},
+		layout = {
+			width = { min = 30 },
+		},
+	})
 
-  require("which-key").add({
-    { "<leader><tab>", group = "tab" },
-    { "<leader>a", group = "ai" },
-    { "<leader>c", group = "code" },
-    { "<leader>d", group = "debug" },
-    { "<leader>dL", group = "debug lua" },
-    { "<leader>n", group = "notes" },
-    { "<leader>g", group = "git" },
-    { "<leader>gb", group = "blame" },
-    { "<leader>gd", group = "diffview" },
-    { "<leader>gh", group = "hunks" },
-    { "<leader>r", group = "run" },
-    { "<leader>s", group = "search" },
-    { "<leader>t", group = "test" },
-    { "<leader>u", group = "ui" },
-    { "<leader>x", group = "diagnostics/quickfix" },
-    { "<leader>w", group = "windows", proxy = "<C-w>" },
-    {
-      "<leader>b",
-      group = "buffers",
-      expand = function()
-        return require("which-key.extras").expand.buf()
-      end,
-    },
-  })
+	require("which-key").add({
+		{ "<leader><tab>", group = "tab" },
+		{ "<leader>a", group = "ai" },
+		{ "<leader>c", group = "code" },
+		{ "<leader>d", group = "debug" },
+		{ "<leader>dL", group = "debug lua" },
+		{ "<leader>n", group = "notes" },
+		{ "<leader>g", group = "git" },
+		{ "<leader>gb", group = "blame" },
+		{ "<leader>gd", group = "diffview" },
+		{ "<leader>gh", group = "hunks" },
+		{ "<leader>r", group = "run" },
+		{ "<leader>s", group = "search" },
+		{ "<leader>t", group = "test" },
+		{ "<leader>u", group = "ui" },
+		{ "<leader>x", group = "diagnostics/quickfix" },
+		{ "<leader>w", group = "windows", proxy = "<C-w>" },
+		{
+			"<leader>b",
+			group = "buffers",
+			expand = function()
+				return require("which-key.extras").expand.buf()
+			end,
+		},
+	})
 end)

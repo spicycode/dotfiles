f6e8cbcb68ca7912efcd172d64f8d98cd2399f87
Chad Humphries <chad@humphries.sh>
2026-06-16 18:33:16 -0700

    fixes to blink and octo, drop showkeys

--- .config/nvim/plugin/showkeys.lua
+++
@@ -1,7 +0,0 @@
-require("lazyload").on_vim_enter(function()
-  vim.pack.add({
-    { src = "https://github.com/nvzone/showkeys" },
-  })
-
-  vim.keymap.set("n", "<leader>uk", "<cmd>ShowkeysToggle<CR>", { desc = "Toggle showkeys", silent = true })
-end)
